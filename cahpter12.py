81656bfdcfcc8466765a54c91e4d134f7da3ff30
Kew Forest Teacher <daronson@kewforest.org>
2018-04-25 10:34:24 -0400

    new

---
+++ cahpter12.py
@@ -0,0 +1,5 @@
+import seq_tools
+
+s ="A string"
+s=seq_tools.remove_at(4, s)
+print(seq_tools.mystring)
---
+++ screen.py
@@ -0,0 +1,17 @@
+import pygame
+(width,height)=(600,500)
+screen = pygame.display.set_mode((width, height))
+pygame.display.flip()
+b
+while True:
+    pass
+running = True
+
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT():
+            running = False
+
+            m   
