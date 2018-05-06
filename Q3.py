5b71ddda88d8a072f36efd07552d966dbfb36e04
Taneil-Kew <31958887+Taneil-Kew@users.noreply.github.com>
2018-05-06 17:58:03 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,9 +1,16 @@
 #question 3
+def find(strng, ch, start=0):
+    index = start
+    while index < len(strng):
+          if strng[index] == ch:
+              return index
+          index += 1
+    return -1
 
-def count_letters(fruit,count):
-    fruit = "banana"
+def count_letters(word, letter):
+    a    = find(word, letter, 0)
     count = 0
-    for char in fruit:
-        if char == "a":
-            count += 1
-    print(count)
+    while a != -1:
+        a    = find(word, letter, a+1)
+        count += 1
+    return count
